9614eaecc36ecd4882bc9d1c6ccef312d97c53f0
Dima <78323304+DimaFursov86@users.noreply.github.com>
2021-05-19 02:22:28 +0300

    added up to 28 tasks

---
+++ src/js-2.24.js
@@ -0,0 +1,23 @@
+function checkFruit(fruit) {
+  const fruits = ["apple", "plum", "pear", "orange"];
+
+  return fruits.includes(fruit);
+}
+
+console.log(checkFruit("plum"));
+
+// Функция checkFruit(fruit) принимает строку с названием фрукта (параметр fruit), и проверяет есть ли такой фрукт в массиве fruits.
+
+// Дополни код функции так, что если:
+
+// фрукт есть в массиве, то функция возвращает true;
+// фрукта нет в массиве, то функция возвращает false.
+// Тесты
+// Объявлена функция checkFruit(fruit)
+// Вызов checkFruit("plum") возвращает true
+// Вызов checkFruit("mandarin") возвращает false
+// Вызов checkFruit("pear") возвращает true
+// Вызов checkFruit("Pear") возвращает false
+// Вызов checkFruit("apple") возвращает true
+// Вызов функции checkFruit() со случайным словом возвращает правильное значение boolean
+// В функции использовался метод includes
